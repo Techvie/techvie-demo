a23ec47f7bd077c5a24877f6592677bab87955fa
Dinesh <petadineshkumar97@gmail.com>
2019-07-05 12:49:43 +0530

    commit

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ mongoose.Promise = global.Promise;
 
 app.use('/api',Posts)
 
+// Serve static assets if in production
+
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const PORT= process.env.PORT || 5000;
 

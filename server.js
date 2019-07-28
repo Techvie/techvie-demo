596b2700984c78806b48b75c36b7ddbe1a4c57ea
Dinesh <petadineshkumar97@gmail.com>
2019-07-28 11:07:04 +0530

    commit

--- server.js
+++ server.js
@@ -21,27 +21,14 @@ app.use('/api',Posts)
 
 // Serve static assets if in production
 
-// if (process.env.NODE_ENV === 'production') {
-//     // Set static folder
-//     app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
   
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-//   }
-
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')))
-  //
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'client/build/index.html'))
-  })
-}
-
-//Route Operations...
-app.get('/', (req, res) => {
-  res.send('Root route of server');
-});
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const PORT= process.env.PORT || 5000;
 
